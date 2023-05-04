6c17c274d3d8aa3af5dd61c0e860012a233237fb
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-05-04 23:00:04 +0300

    ImageGallery fix propTypes

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 
 
 const ImageGallery = ({ images, openModal }) => {
+  console.log(images);
   return (
     <ul className={css.gallery}>
       {images.map(({ id, webformatURL, largeImageURL, tags }) => (
@@ -18,6 +19,11 @@ const ImageGallery = ({ images, openModal }) => {
 export default ImageGallery;
 
 ImageGallery.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.shape(PropTypes.string.isRequired)),
+  images: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string,
+  })),
   openModal: PropTypes.func.isRequired,
 }
